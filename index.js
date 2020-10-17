4001ff3ec60e26470c1f52e5e8207b5a0e91750b
blurryfacebeat <blurryfacebeat@gmail.com>
2020-10-18 01:21:31 +0700

    Теперь reference тоже создается

--- index.js
+++ index.js
@@ -1,71 +1,130 @@
 // JSON на входе
 const jsonInput2 = `
 {
-    "name":"addpost",
+    "name":"interview",
     "fields":[
-    {
-        "label":"Title",
-        "input": {
-            "type":"text",
-            "required": true,
-            "placeholder": "Эщкере, ребят"
+        {
+            "label":"Введите своё ФИО",
+            "input":{
+                "type":"text",
+                "required":true,
+                "placeholder":"Иванов Иван Иванович"
+            }
+        },
+        {
+          "label":"Введите Номер телефона",
+          "input":{
+              "type":"number",
+              "required":true,
+              "mask": "+7 (999) 99-99-999"
+          }
+        },
+        {
+          "label":"Введите свою Почту",
+          "input":{
+              "type":"email",
+              "required":true,
+              "placeholder":"example@mail.com"
+          }
+        },
+        {
+            "label":"Введите свой возраст",
+            "input":{
+                "type":"number",
+                "required":true
+            }
+        },
+        {
+            "label":"Введите вашу специальность",
+            "input":{
+                "type":"text",
+                "required":true
+            }
+        },
+        {
+            "label":"Выберете технологии, с которыми вы работали",
+            "input":{
+                "type":"technology",
+                "required": true,
+                "technologies": ["PHP", "JS", "Laravel", "Express.js", "Yii2", "HTML", "CSS", "Java"],
+                "multiple": true
+            }
+        },
+        {
+            "label":"Ваш срок работы",
+            "input":{
+                "type":"number",
+                "required": true
+            }
+        },
+        {
+            "label":"Ваша фотография",
+            "input":{
+                "type":"file",
+                "required":true
+            }
+        },
+        {
+            "label":"Серия, номер",
+            "input":{
+                "type": "number",
+                "required": true,
+                "mask": "99-99 999999"
+            }
+        },
+        {
+            "label":"Код подразделения",
+            "input":{
+                "type": "number",
+                "required": true,
+                "mask": "999-999"
+            }
+        },
+        {
+            "label":"Скан / Фото паспорта (1 страница)",
+            "input":{
+                "type": "file",
+                "required": true,
+                "multiple": true,
+                "filetype": ["png", "jpeg", "pdf"]
+            }
+        },
+        {
+            "label":"Расскажите немного о себе",
+            "input":{
+                "type":"textarea",
+                "required:":true
+            }
         }
-    },
-    {
-        "label":"Description",
-        "input": {
-            "type":"textarea",
-            "required":true
+    ],
+    "references":[
+        {
+          "input":{
+            "type":"checkbox",
+            "required":true,
+            "checked":false
+          }
+        },
+        {
+            "text without ref":"I accept the",
+            "text":"Terms & Conditions",
+            "ref":"termsandconditions"
         }
-    },
-    {
-        "label":"Image",
-        "input": {
-            "type":"file",
-            "required": true
-        }
-    },
-    {
-        "label":"Publish Date",
-        "input": {
-            "type": "date",
-            "required": true
-        }
-    },
-    {
-        "label": "Author",
-        "input": {
-            "type": "text"
-        }
-    }
-],
-"references":[
-    {
-      "input":{
-        "type":"checkbox",
-        "required":true,
-        "checked":"false"
-      }
-    },
-    {
-        "text without ref":"View Author Post",
-        "text":"View Author Post",
-        "ref":"viewauthor"
-    }
-  ],
+    ],
     "buttons":[
-    {
-        "text":"Create Post"
-    }
-]
+        {
+            "text":"Send"
+        },
+        {
+            "text":"Cancel"
+        }
+    ]
 }
 `
 // Контейнер для формы
 const $app = document.getElementById('app');
 
 const temp2 = JSON.parse(jsonInput2);
-
-console.log(temp2);
 
 // Компоненты
 const components = {
@@ -76,33 +135,58 @@ const components = {
         open: '<input class="form-control"',
         inputTypeOpen: ' type="',
         inputTypeClose: '"',
+        typeTechnology: 'select',
         placeholderOpen: ' placeholder="',
         placeholderClose: '"',
         required: ' required',
+        multiple: ' multiple',
+        filetypeOpen: ' accept="',
+        filetypeClose: '"',
         close: '>',
         containerClose: '</div>'
     },
     reference: {
         containerOpen: '<div class="form-group">',
-        labelOpen: '<label class="form-label">',
-        labelClose: '</label>',
-        open: '<input type="checkbox" class="form-control"',
-        required: ' required',
+        checkboxOpen: '<input type="checkbox"',
+        checkboxClose: '>',
         checked: ' checked',
-        close: '>',
+        required: ' required',
+        textOpen: '<span>',
+        textClose: '</span>',
+        linkOpen: '<a',
+        hrefOpen: ' href="',
+        hrefClose: '">',
+        linkClose: '</a>',
         containerClose: '</div>'
     },
     button: {
-        open: '<button>',
+        open: '<button class="form__button">',
         close: '</button>'
     }
 };
-
+// Создать элементы options
 function buildField(field) {
     let newField = components.field.containerOpen;
 
     if (field.label) {
         newField += components.field.labelOpen + field.label + components.field.labelClose;
+    }
+
+    if (field.input.type === 'technology') { 
+        newField += components.field.open + components.field.inputTypeOpen + components.field.typeTechnology + components.field.inputTypeClose;
+
+        if (field.input.required === true) {
+            newField += components.field.required;
+        }
+
+        if (field.input.multiple === true) {
+            newField += components.field.multiple;
+        }
+
+        newField += components.field.close;
+        newField += components.field.containerClose;
+
+        return newField;
     }
 
     newField += components.field.open + components.field.inputTypeOpen + field.input.type + components.field.inputTypeClose;
@@ -115,13 +199,55 @@ function buildField(field) {
         newField += components.field.required;
     }
 
+    if (field.input.multiple === true) {
+        newField += components.field.multiple;
+    }
+
+    if (field.input.filetype) {
+        let temp = '';
+        for (let i = 0; i < field.input.filetype.length; i++) {
+            if (i === field.input.filetype.length - 1) {
+                temp += field.input.filetype[i];
+                break;
+            }
+            temp += field.input.filetype[i] + ', ';
+        }
+        newField += components.field.filetypeOpen + temp + components.field.filetypeClose;
+    }
+
     newField += components.field.close;
     newField += components.field.containerClose;
     return newField;
 }
 
 function buildReference(field) {
+    let newReference = components.reference.containerOpen;
 
+    if (field.input) {
+        newReference += components.reference.checkboxOpen;
+
+        if (field.input.required === true) {
+            newReference += components.reference.required;
+        }
+
+        if (field.input.checked === true) {
+            newReference += components.reference.checked;
+        }
+
+        newReference += components.reference.checkboxClose;
+    }
+
+    if (field['text without ref']) {
+        newReference += components.reference.textOpen + field['text without ref'] + components.reference.textClose;
+    }
+
+    if (field.text && field.ref) {
+        newReference += components.reference.linkOpen + components.reference.hrefOpen + field.ref + components.reference.hrefClose + field.text + components.reference.linkClose;
+    }
+
+    newReference += components.reference.containerClose;
+
+    return newReference;
 }
 
 function buildButton(field) {
@@ -145,16 +271,13 @@ function buildJSONForm(json) {
 
         if (fieldName === 'fields') {
             for (let i = 0; i < currentKey.length; i++) {
-                // console.log(buildField(currentField[i]));
                 form += buildField(currentKey[i]);
             }
         }
 
         if (fieldName === 'references') {
             for (let i = 0; i < currentKey.length; i++) {
-                console.log(111);
-                // console.log(currentField[i]);
-                // form += buildField(currentField[i]);
+                form += buildReference(currentKey[i]);
             }
         }
 
